23ca889c02236ab1087f1615603374c55eb129ba
Gülcan <83371233+gulcanrabiaaksu@users.noreply.github.com>
2022-09-16 00:17:24 +0300

    .

--- proje4/src/main/java/proje4/Proje4.java
+++ proje4/src/main/java/proje4/Proje4.java
@@ -1,17 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package proje4;
 
-/**
- *
- * @author glcn
- */
+import java.util.Random;
+
 public class Proje4 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for (int i=0;i<20;i++)
+        {
+            Random rng=new Random();
+            System.out.println(rng.nextInt(2)); //rng.nextInt() icine tanimlanilan deger hangi sayilarin olacagini gosterir
+                                                       //Ornegin 1 koyarsak sadece 0, 2 koyarsak 0 ve 1 i alacaktir.
+        }
     }
 }
